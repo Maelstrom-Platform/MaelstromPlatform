053cff6f96f9c580aaf630c784cdb3cb82a07993
Jason Alan Smith <jason.alan.smith@outlook.com>
2024-08-03 10:20:57 -0500

    Add data grid to IssueBrowse component
    Also setup IssueItemDashboard and IssueItemProfile components to use
    Id parameter from the IssueBrowse component and load from API the
    selected issue in the IssueBrowse component. Created API endpoint to
    get just one issue.
    
    This adds technical debt as a generic data grid was not finished, but
    I'm willing to accept this in the spirit of getting many other parts
    of the Maelstrom Platform implemented sooner than later.
    
    Fixes #44.

--- src/MaelstromPlatform.API/Issue/IssueProfile.cs
+++ src/MaelstromPlatform.API/Issue/IssueProfile.cs
@@ -7,6 +7,7 @@ namespace MaelstromPlatform.API.Issue
         public IssueProfile()
         {
             CreateMap<IssueEntity, IssueForGetAllDto>().ReverseMap();
+            CreateMap<IssueEntity, IssueForGetByIdDto>().ReverseMap();
         }
     }
 }
